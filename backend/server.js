f5114cfea614aaff2d669948c7d3f0de866f0e3a
Parthawe <130175802+Parthawe@users.noreply.github.com>
2024-09-05 20:31:48 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../frontend/build')));
@@ -20,3 +24,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
